6cf9841c6a217f07fad7e9b7d8d1de3ed645fbd2
Wasim <wasimbhai6205@gmail.com>
2023-09-17 06:28:15 -0700

    String

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "wasim "
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("wasimmm-bca");
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf("s"));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-4,4)
+// console.log(anotherString);
+
+// const newStringOne = "    wasim   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+// const url = "https://wasim.com/wasim%20ansari"
+// console.log(url.replace("%20", "-"));
+
+// console.log(url.includes("one"));
+// console.log(url.includes("wasim"));
+
+console.log(gameName.split("-"));
