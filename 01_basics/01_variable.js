ca28b7809229cd8b05805b3c7281ef15c3f3d713
Wasim <wasimbhai6205@gmail.com>
2023-09-16 22:57:50 -0700

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "wasim1@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2 not allowed
+accountEmail = "hc2hc.com"
+accountPassword = "2121212"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
