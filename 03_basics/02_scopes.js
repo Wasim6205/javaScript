40b5ecfdb6e5c95e4e21b3a7f5a77876480a034c
Wasim <wasimbhai6205@gmail.com>
2023-09-20 07:06:08 -0700

    function,arrowFunction,controlFlow

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,52 @@
+// var c = 300
+let a = 300
+
+if (true) {
+  let a = 10;
+  const b = 20;
+//   console.log("INNER ", a);
+}
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "wasim"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    two()
+}
+
+// one()
+
+
+if(true){
+    const username = "wasim"
+    if(username === "wasim"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// +++++++++++++++++++++ intersting ++++++++++++++++++++++++++
+
+addone(5)
+function addone(num){
+    return num +1;
+}
+
+
+const addTwo = function(num){
+    return num + 2
+}
+
+addTwo(5)
